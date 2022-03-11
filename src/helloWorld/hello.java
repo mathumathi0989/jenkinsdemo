d478e1a7c224dcd8f5351c04e470958fac090a97
Mathumathi Balakrishnan <mathumathi.b@gmail.com>
2022-03-10 21:40:39 -0500

    first jenkins demo class

---
+++ src/helloWorld/hello.java
@@ -0,0 +1,11 @@
+package helloWorld;
+
+public class hello {
+
+	public static String name = "Mathumathi";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello! My name is " +name);
+	}
+
+}
